f619d7d3df4ef2e73be421d76d4c582c394e11d6
LuckyPunk <moutafis@tem.uoc.gr>
2014-01-27 22:56:17 +0200

    kainourgio update.php

--- update.php
+++ update.php
@@ -1,3 +1,38 @@
 <?php
-    echo "This is the update script";
+    if(isset($_POST['cases'])) {
+        $case_value = $_POST['cases'];
+        if($case_value=="B_P"){
+            echo "Βουλευτική Περίοδος";
+        }
+        elseif($case_value=="Party"){
+            echo "Κόμμα";
+        }
+        elseif($case_value=="Con_Info"){
+            echo "Πληροφορίες Βουλευτών";
+        }
+        elseif($case_value=="Gov"){
+            echo "Κυβέρνηση";
+        }
+        elseif ($case_value=="Per_Info") {
+            echo "Πληροφορίες Ατόμων";
+        }
+    }
+    
 ?>
+
+<html>
+    <head></head>
+    <body>
+        <form action="update.php" method="post">
+            <select name="cases" >
+                <option value="def" >Επιλέξτε</option>
+                <option value="B_P" >Βουλευτική Περίοδος</option>
+                <option value="Party" >Κόμμα</option>
+                <option value="Con_Info" >Πληροφορίες Βουλευτών</option>
+                <option value="Gov" >Κυβέρνηση</option>
+                <option value="Per_Info" >Πληροφορίες Ατόμων</option>
+            </select> 
+            <input type="submit" name="case_submit" value="Submit" />           
+        </form>    
+    </body>    
+</html>
